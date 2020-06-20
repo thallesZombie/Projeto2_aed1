e17a8471f0c2c4eda0543e5d72967391cbf7df98
thalles rufino <thalles027@ucl.br>
2020-06-20 14:28:29 +0000

    case2

--- main.cs
+++ main.cs
@@ -1,4 +1,4 @@
-using System;
+ using System;
 using System.IO;
 using System.Text;
 
@@ -177,9 +177,224 @@ class MainClass {
        
       break;
 /* //////////////*/
+      case 2:
+        Console.WriteLine("Digite 3 para fazer um cadastro no nosso Sistema:\nDigite 4 se você já é cadastrado:");
+      opcao = int.Parse(Console.ReadLine());
+        if(opcao == 2){
 
+        Console.WriteLine("Digite 1 para comprar\n Digite 2 para olhar seu historico:");
+        int opcaoUsuarioF = int.Parse(Console.ReadLine());
 
-  
+        Fisico  usuario = new Fisico();
+
+        Console.WriteLine("Digite seu email:");
+        string  email = (Console.ReadLine());
+
+        Console.WriteLine("Digite sua senha:");
+        string  senha = (Console.ReadLine());
+
+        Console.WriteLine();
+
+        if(usuario.Login(email,senha)){
+
+        int opcaoUsuario = 0;
+
+        Console.WriteLine();
+
+       
+        if(opcaoUsuarioF == 1){
+
+          Loja loja = new Loja();
+
+          Console.WriteLine("Esse são todos os nossos produtos para venda:");
+
+          loja.MostrarProduto();
+          Console.ForegroundColor = ConsoleColor.White;
+          Console.WriteLine();
+
+          Console.WriteLine("Digite o ID do produto que você deseja comprar:");
+          int   id  = int.Parse(Console.ReadLine());
+        
+          if(loja.BuscarPorID(id)){
+       
+          Console.WriteLine();
+          Console.WriteLine("Agora que você escolheu o seu ID, coloque a quantidade de produtos que você quer comprar:");
+          int   quantidade = int.Parse(Console.ReadLine());
+
+          historicoFisico.ColocandoNoHistorico(email,id,quantidade);
+
+          loja.ComprarProduto(id,quantidade);
+          Console.WriteLine("Se deseja comprar mais produto, digite aperte o s");
+          string  desejacompra = Console.ReadLine();
+
+          string desejacompra1 ="";
+
+          while(desejacompra == "s"|| desejacompra1 == "s"){
+               
+            if(desejacompra == "s"){
+        
+            Console.WriteLine("Digite o ID do produto que você deseja comprar:");
+            int   id1  = int.Parse(Console.ReadLine());
+            
+            if(loja.BuscarPorID(id1)){
+        
+            Console.WriteLine("Agora que você escolheu o seu ID, coloque a quantidade de produtos que você quer comprar:");
+            int   quantidade1 = int.Parse(Console.ReadLine());
+
+            loja.ComprarProduto(id1,quantidade1);
+
+            historicoFisico.ColocandoNoHistorico(email,id1,quantidade1); 
+            }
+          }
+          if(desejacompra != "s"){
+              break;
+          }
+
+          Console.WriteLine("Se deseja comprar mais produto, digite aperte o s");
+          desejacompra1 = Console.ReadLine();
+
+          if(desejacompra1 != "s"){
+            break;
+            }
+        }
+
+        }
+        else{
+          Console.WriteLine("ID errado, tente novamente!!!");
+        } 
+      
+        }
+        if(opcaoUsuarioF == 2){
+          HistoricoFisico historico = new HistoricoFisico();
+          Console.WriteLine("Esse é todo o seu historico em nossa loja");
+          historico.MostrarHistorico(email);
+
+        }
+        if(opcaoUsuario == 89){
+          break;
+        }
+        }  
+      }
+      if(opcao == 3){
+
+      Console.WriteLine("Digite seu nome para cadastro:");
+      string nomeJ  = (Console.ReadLine());
+
+      Console.WriteLine("Digite seu email para cadastro:");
+      string  emailJ = (Console.ReadLine());
+      
+      Console.WriteLine("Digite seu cep:");
+      string  cepJ = (Console.ReadLine());
+
+      Console.WriteLine("Digite sua senha para cadastrar:");
+      string  senhaJ  = (Console.ReadLine());
+
+      Console.WriteLine("Digite sua cnpj para cadastrar:");
+      string  cnpj  = (Console.ReadLine());
+      
+      Juridico  usuarioJ = new Juridico(nomeJ,emailJ,cepJ,senhaJ,cnpj);
+
+      usuarioJ.CadastroUsuarioJuridico2(nomeJ, emailJ,cepJ,senhaJ,cnpj);
+      Console.ForegroundColor = ConsoleColor.White; 
+      break;
+      }
+
+      if(opcao == 4){
+        Console.WriteLine("Digite 1 para comprar\n Digite 2 para olhar seu historico:");
+        int opcaoUsuarioJ = int.Parse(Console.ReadLine());
+
+        Juridico  usuarioJ = new Juridico();
+
+        Console.WriteLine("Digite seu email:");
+        string  emailJ = (Console.ReadLine());
+
+        Console.WriteLine("Digite sua senha:");
+        string  senhaJ= (Console.ReadLine());
+
+        Console.WriteLine();
+
+        if(usuarioJ.Login2(emailJ,senhaJ)){
+
+        int opcaoUsuario = 0;
+
+        Console.WriteLine();
+
+       
+        if(opcaoUsuarioJ == 1){
+
+          Loja lojaJ = new Loja();
+
+          Console.WriteLine("Esse são todos os nossos produtos para venda:");
+
+          lojaJ.MostrarProduto();
+          Console.ForegroundColor = ConsoleColor.White;
+          Console.WriteLine();
+
+          Console.WriteLine("Digite o ID do produto que você deseja comprar:");
+          int   idJ  = int.Parse(Console.ReadLine());
+        
+          if(lojaJ.BuscarPorID(idJ)){
+       
+          Console.WriteLine();
+          Console.WriteLine("Agora que você escolheu o seu ID, coloque a quantidade de produtos que você quer comprar:");
+          int   quantidadeJ = int.Parse(Console.ReadLine());
+
+          historicoJuridico.ColocandoNoHistorico2(emailJ,idJ,quantidadeJ);
+
+          lojaJ.ComprarProduto(idJ,quantidadeJ);
+
+          Console.WriteLine("Se deseja comprar mais produto, digite aperte o s");
+          string  desejacompraJ = Console.ReadLine();
+
+          string desejacompraJ2 ="";
+        
+
+          while(desejacompraJ == "s"){
+               
+            if(desejacompraJ2 == "s"){
+             
+            Console.WriteLine("Digite o ID do produto que você deseja comprar:");
+            int   idJ1  = int.Parse(Console.ReadLine());
+            
+            if(lojaJ.BuscarPorID(idJ1)){
+        
+            Console.WriteLine("Agora que você escolheu o seu ID, coloque a quantidade de produtos que você quer comprar:");
+            int   quantidadeJ1 = int.Parse(Console.ReadLine());
+
+            lojaJ.ComprarProduto(idJ1,quantidadeJ1);
+
+            historicoJuridico.ColocandoNoHistorico2(emailJ,idJ1,quantidadeJ1); 
+            }
+          }
+          if(desejacompraJ != "s"){
+              break;
+          }
+
+          Console.WriteLine("Se deseja comprar mais produto, digite aperte o s");
+          desejacompraJ2 = Console.ReadLine();
+
+          if(desejacompraJ2 != "s"){
+            break;
+            }
+        }
+
+        }
+        else{
+          Console.WriteLine("ID errado, tente novamente!!!");
+        } 
+      
+        }
+        if(opcaoUsuarioJ == 2){
+          HistoricoJuridico historicoJ = new HistoricoJuridico();
+          Console.WriteLine("Esse é todo o seu historico em nossa loja");
+          historicoJ.MostrarHistorico2(emailJ);
+
+        }
+        if(opcaoUsuarioJ == 89){
+          break;
+        }
+        }  
+      }
 
       break;
 
@@ -233,3 +448,8 @@ class MainClass {
     return true;  
   }
  }
+ 
+ 
+  
+
+ 
